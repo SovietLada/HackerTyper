4c76afe14aa02ce623f6e3064ed52f68f8d01ff8
Leevi Heino <leeviheino@live.fi>
2018-06-18 08:23:20 +0300

    small fix

--- TextDisplay.js
+++ TextDisplay.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Text, Button, View, StatusBar, StyleSheet } from 'react-native'
 
+const firstLine = '<init hacker prompt>\n'
 const line1 = 'import HackSuite from \'FBINetworkPkg\''
 const line2 = '<View>\n    { parseBankPin }\n</View>'
 const line3 = '<Container>\n    <BreachTool>\n        { illegalExpression }\n    </BreachTool>\n    {activateBotnet}\n</Container>'
@@ -8,8 +9,6 @@ const line4 = 'componentDidMount() {\n    this.displayAnonLogo()\n    HackSuite.
 const line5 = 'import Heartbleed from \'BelarusNetworkPack\''
 const line6 = 'serializeData() {\n    buffer = 00<<bitstream\n    commit(inout buffer)\n    return buffer.clear()\n}'
 const line7 = 'setDelta() {\n    this.step += hackbits\n}'
-
-const firstLine = '<init hacker prompt>\n'
 const hacks = [line1, line2, line3, line4, line5, line6, line7]
 const kFontSize = 9
 const kRed = '#d43535'
